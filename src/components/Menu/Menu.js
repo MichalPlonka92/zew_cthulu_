cc0a7aee926adf30bf7f4c7a58a737d5b9072e4e
Michał Płonka <mplonka1992@gmail.com>
2023-06-07 17:43:23 +0200

    menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,16 +8,14 @@ const Menu = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">My Simple Site</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">Zew Cthulhu</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link >
-                            <Link to="/">Home</Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to="/about">About</Link>
-                        </Nav.Link>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/create-traveler">Create traveler</Nav.Link>
+                        <Nav.Link as={Link} to="/traveler-card">Traveler card</Nav.Link>
+                        <Nav.Link as={Link} to="/description">Description</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
